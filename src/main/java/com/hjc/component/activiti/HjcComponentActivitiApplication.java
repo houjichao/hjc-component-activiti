a75543d11c891f35023c69e1da2e304815bb4533
houjichao <houjichao@netposa.com>
2019-08-01 18:58:27 +0800

    【提交备注】spring boot 整合activiti工作流

---
+++ src/main/java/com/hjc/component/activiti/HjcComponentActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.hjc.component.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjcComponentActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjcComponentActivitiApplication.class, args);
+    }
+
+}
